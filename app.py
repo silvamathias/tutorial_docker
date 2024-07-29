e4ef8fdd72a2fd4644e0e90f5bacf22639024696
Mathias <phelipe.mathias@capgemini.com>
2024-07-29 18:00:25 -0300

    correcao do app python

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ import brazilian_register as br
 def decorador(func):
     def validacao(tipo,doc):
         f = func(tipo,doc)
-
         if f == False:
            return print('O documento {} informado é inválido'.format(doc))
         
-        else:           
+        elif f == True:           
            return print('O documento {} informado é válido'.format(doc))
     
     return validacao
@@ -18,16 +17,23 @@ def decorador(func):
 @decorador
 def valida_doc(tipo, doc):
     
-    if tipo == 0:
+    if tipo == '0':
         r = br.cpf(doc)
 
         return r.cpf_status()
     
-    elif tipo == 1:
+    elif tipo == '1':
         r = br.cnpj(doc)
 
         return r.cnpj_status()
 
+
+# %%
+t = br.cpf('05647684726')
+
+d = t.cpf_status()
+
+print(d)
 
 # %%
 tipo = input('digite 0 para CPF e 1 para CNPJ:')
