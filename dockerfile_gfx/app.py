9aab152932207c726e4279e661075866c1805c3b
mathias <silvamathias@gmail.com>
2024-08-02 01:22:15 -0300

    troca do app python

---
+++ dockerfile_gfx/app.py
@@ -0,0 +1,26 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as mp 
+
+
+# %%
+a = np.linspace(0,4*np.pi,400)
+
+# %%
+y1 = np.sin(a)
+
+# %%
+y2 = np.cos(a)
+
+# %%
+mp.plot(a,y1)
+mp.plot(a,y2)
+mp.show()
+
+# %%
+fig, gx = mp.subplots(2,1)
+gx[0].plot(a,y1)
+gx[1].plot(a,y2)
+mp.show()
+
+
